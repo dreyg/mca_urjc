c15d80f80490aaab4fb009c6fc9d9e560e1f559f
juaneb <juanchi.kiuxus@gmail.com>
2021-02-12 22:11:44 +0100

    Apartado 1 Práctica 2

--- modulo_2_SI/2.3/prac2_bddMultiModelo_noRel/prac2_jeb_drg/src/main/resources/db/migration/V1__inicio.sql
+++ modulo_2_SI/2.3/prac2_bddMultiModelo_noRel/prac2_jeb_drg/src/main/resources/db/migration/V1__inicio.sql
@@ -5,7 +5,7 @@
 
 CREATE TABLE `hibernate_sequence` (
     `next_val` bigint DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB;
 
 
 -- test.avion definition
